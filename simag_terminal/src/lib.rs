f0cdaacd8c155cb74366ec31038224863e64ffaf
Ignacio Duart <iduartgomez@gmail.com>
2021-01-23 14:22:06 +0100

    make cursor feature specific

--- simag_terminal/src/lib.rs
+++ simag_terminal/src/lib.rs
@@ -1,10 +1,9 @@
 mod action;
-pub(crate) mod cursor;
 mod interpreter;
 #[cfg(all(target_family = "unix", feature = "termion"))]
-mod terminal;
+mod unix;
 
 pub use action::Action;
 pub use interpreter::{ReplInterpreter, SimagInterpreter};
 #[cfg(all(target_family = "unix", feature = "termion"))]
-pub use terminal::Terminal;
+pub use unix::terminal::Terminal;
