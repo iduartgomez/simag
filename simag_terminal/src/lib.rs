27c65994d4c86d16d40e0b2bad448c6891baa1a8
Ignacio Duart <iduartgomez@gmail.com>
2021-01-23 14:00:29 +0100

    refine further platform specific features

--- simag_terminal/src/lib.rs
+++ simag_terminal/src/lib.rs
@@ -1,10 +1,10 @@
 mod action;
 pub(crate) mod cursor;
 mod interpreter;
-#[cfg(target_family = "unix")]
+#[cfg(all(target_family = "unix", feature = "termion"))]
 mod terminal;
 
 pub use action::Action;
 pub use interpreter::{ReplInterpreter, SimagInterpreter};
-#[cfg(target_family = "unix")]
+#[cfg(all(target_family = "unix", feature = "termion"))]
 pub use terminal::Terminal;
