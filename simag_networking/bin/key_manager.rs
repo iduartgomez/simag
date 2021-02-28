628e4bed8f5fbe758f357d1edfa57989b9b2e9ea
Ignacio Duart <iduartgomez@gmail.com>
2021-02-28 14:36:25 +0100

    disallow keygen tool temp

--- simag_networking/bin/key_manager.rs
+++ simag_networking/bin/key_manager.rs
@@ -1,13 +1,13 @@
-use ed25519_dalek::Keypair;
 use rand::rngs::OsRng;
 use std::fs::File;
 use std::io::Write;
 
+#[allow(unreachable_code, unused_variables)]
 fn main() {
-    let mut csprng = OsRng {};
-    let keypair: Keypair = Keypair::generate(&mut csprng);
+    let csprng = OsRng {};
+    let keypair: &[u8] = { todo!() };
 
     let secret_file = std::env::current_dir().unwrap().join("bootstrap_secret");
     let mut file = File::create(secret_file).unwrap();
-    file.write_all(&keypair.to_bytes()).unwrap();
+    file.write_all(&keypair).unwrap();
 }
